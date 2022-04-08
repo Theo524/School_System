de7cdb10846d98c62deab19f328c5925e8b4f368
Theo524 <theophilusbrownjacob524@gmail.com>
2022-04-08 09:54:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 from SchoolSystem import school_system
-from MovieManager import movies
 # code
-choice = input('school system or movie manager?')
-
-school = ['School system', '1', 'school']
-movie = ['movie manager', '2', 'movie ']
-
-if choice in school:
-    school_system.loginMenu()
-
-if choice in movie:
-    movies.loginMenu()
+school_system.loginMenu()
